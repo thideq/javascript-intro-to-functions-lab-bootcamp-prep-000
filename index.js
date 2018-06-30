e13a2b8b2075b1fd6144b5f493abbdb886f623b6
Thierry Dequin <thierrydequin@hotmail.com>
2018-06-30 22:34:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 function shout(string) {
     return string.toUpperCase()
 }
-function sayHiToGrandma()
+function whisper(string) {
+    return string.toLowerCase()
+}
+function logShout(string) {
+    console.log(shout(sting))
+}
+function logWhisper(string) {
+    console.log(whisper(sting))
+}
+var upperCase
+function sayHiToGrandma() {
+  if  (uppercase.toUpperCase() == uppercase) {
+    return "I can't hear you!"
+  }
+  else if (upperCase == "I love you, Grandma.") {
+    return "I love you, too"
+  }
+
+  else {
+    return "YES INDEED!"
+  }
+}
