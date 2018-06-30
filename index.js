657678cba3ebd686196bb240afb73d0b6b9fad79
Thierry Dequin <thierrydequin@hotmail.com>
2018-06-30 22:55:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function whisper(string) {
     return string.toLowerCase()
 }
 function logShout(string) {
-    console.log(shout(string))
+    console.log(string.toUpperCase())
 }
 function logWhisper(string) {
     console.log(string.toLowerCase())
